f341b4c13648f147b35b107aef754d340fafa5ec
Kevin Carpenter <kevinjcarpenter@gmail.com>
2017-12-19 21:01:48 -0500

    Add SelectorsTest.

---
+++ presentation/src/test/kotlin/me/kevcar/scaffolding/presentation/reducer/SelectorsTest.kt
@@ -0,0 +1,17 @@
+package me.kevcar.scaffolding.presentation.reducer
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class SelectorsTest {
+
+    @Test
+    fun assertingTrue() {
+        assertTrue(true)
+    }
+
+    @Test
+    fun assertingFalse() {
+        assertTrue(false)
+    }
+}
